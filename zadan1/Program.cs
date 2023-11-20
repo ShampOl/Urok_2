48be43721c79099d6073e8de47777cf8c9862815
Olya <lolcap202@gmail.com>
2023-11-21 01:11:08 +0600

    add zadan1

---
+++ zadan1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число: ");
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    if (IsMultiple0f7And23(number))
+    {
+        Console.WriteLine($"{number} кратно 7 и 23");
+    }
+   else
+   {
+    Console.WriteLine($"{number} не кратно одновременно 7 и 23");
+   }
+}
+
+else
+{
+    Console.WriteLine("Неккоректный ввод. Пожалуйста, введите целое число.");
+}
+
+
+static bool IsMultiple0f7And23(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
