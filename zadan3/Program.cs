225120bf393e520a58419b36b4eb8386cf7a3628
Olya <lolcap202@gmail.com>
2023-11-21 01:31:44 +0600

    add zadan3

---
+++ zadan3/Program.cs
@@ -0,0 +1,37 @@
+﻿     {
+        Console.WriteLine("Введите целое число из отрезка [10, 99]:");
+
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number) && number >= 10 && number <= 99)
+        {
+            int maxDigit = GetMaxDigit(number);
+
+            Console.WriteLine($"Наибольшая цифра числа {number} - {maxDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число из отрезка [10, 99].");
+        }
+    }
+
+    static int GetMaxDigit(int num)
+    {
+        int maxDigit = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+
+            if (digit > maxDigit)
+            {
+                maxDigit = digit;
+            }
+
+            num /= 10;
+        }
+
+        return maxDigit;
+    }
+
+
