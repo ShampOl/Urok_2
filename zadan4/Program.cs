d6087df501448a486662c3c6b058f8d875cc1240
Olya <lolcap202@gmail.com>
2023-11-21 01:37:47 +0600

    add zadan4

---
+++ zadan4/Program.cs
@@ -0,0 +1,32 @@
+﻿ {
+        Console.WriteLine("Введите натуральное число N:");
+
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            Console.WriteLine($"Цифры числа {n} через запятую: {GetDigitsSeparatedByComma(n)}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите натуральное число.");
+        }
+    }
+
+    static string GetDigitsSeparatedByComma(int number)
+    {
+        string result = "";
+
+        // Преобразование числа в строку и перебор каждой цифры
+        foreach (char digitChar in number.ToString())
+        {
+            // Добавление цифры в строку результата
+            result += digitChar + ",";
+        }
+
+        // Удаление последней запятой, если она есть
+        if (result.EndsWith(","))
+        {
+            result = result.Remove(result.Length - 1);
+        }
+
+        return result;
+    }
